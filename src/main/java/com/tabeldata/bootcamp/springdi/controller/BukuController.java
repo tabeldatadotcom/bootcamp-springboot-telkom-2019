6d707e83510625222bb8d598ec822b688cb0daa7
dimasm93 <software.dimas_m@icloud.com>
2019-10-23 15:05:37 +0700

    membuat fitur list, delete by id, dan find by id

--- src/main/java/com/tabeldata/bootcamp/springdi/controller/BukuController.java
+++ src/main/java/com/tabeldata/bootcamp/springdi/controller/BukuController.java
@@ -4,7 +4,10 @@ import com.tabeldata.bootcamp.springdi.entity.Buku;
 import com.tabeldata.bootcamp.springdi.repository.BukuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -15,12 +18,26 @@ public class BukuController {
     @Autowired
     private BukuRepository repo;
 
-    @GetMapping(value = "/halo")
-    public Buku halo() {
-        Optional<Buku> bukuOptional = repo.findById(1);
+    @GetMapping(value = "/buku/{id}")
+    public ResponseEntity<?> findById(@PathVariable("id") Integer id) {
+        Optional<Buku> bukuOptional = repo.findById(id);
         if (bukuOptional.isPresent())
-            return bukuOptional.get();
+            return ResponseEntity.ok(bukuOptional.get());
         else
-            return null;
+            return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/buku/list")
+    public Iterable<Buku> listBuku() {
+        return repo.findAll();
+    }
+
+    @DeleteMapping(value = "/buku/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable("id") Integer id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else
+            return ResponseEntity.noContent().build();
     }
 }
