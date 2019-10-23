7a0a79f96de36accc6e192278ea51d71ab7ca81a
dimasm93 <software.dimas_m@icloud.com>
2019-10-23 15:28:57 +0700

    membuat fitur tambah dan update data

--- src/main/java/com/tabeldata/bootcamp/springdi/controller/BukuController.java
+++ src/main/java/com/tabeldata/bootcamp/springdi/controller/BukuController.java
@@ -5,10 +5,7 @@ import com.tabeldata.bootcamp.springdi.repository.BukuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -39,5 +36,11 @@ public class BukuController {
             return ResponseEntity.ok().build();
         } else
             return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/buku")
+    public ResponseEntity<?> save(@RequestBody Buku buku) {
+        buku = repo.save(buku);
+        return ResponseEntity.ok(buku);
     }
 }
